f21c56a9949774cfc7b52cdf2cf834fc5c611ad0
bcorman <benjamincorman@gmail.com>
2018-08-08 12:05:38 -0700

    adding setup form

--- src/components/forms/InitForm.js
+++ src/components/forms/InitForm.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react'
+import { Form, Button, Dropdown } from 'semantic-ui-react'
 
 class InitForm extends Component {
   render() {
     return (
+      <Form>
+        <Form.Field>
+          <label>Tournament Name</label>
+          <input placeholder='Tournament Name' />
+        </Form.Field>
+        <Form.Field>
+          <label>Tournament Date</label>
+          <input placeholder='Tournament Date' />
+        </Form.Field>
+        <Form.Field>
+          <label>Number of Rounds</label>
+          <Dropdown options={[1, 2, 3, 4, 5]} />
+        </Form.Field>
+        <Form.Field>
+          <label>Tournament Location</label>
+          <input placeholder='Tournament Location' />
+        </Form.Field>
 
+        <Button type='submit'>Submit</Button>
+      </Form>
     )
   }
 }
--- src/pages/InitializationPage.js
+++ src/pages/InitializationPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import InitForm from '../components/forms/InitForm'
 import './InitializationPage.css'
 
 class InitializationPage extends Component {
@@ -7,7 +8,7 @@ class InitializationPage extends Component {
       <div className="init-page">
         <h3>Setup</h3>
         <p>To begin setting up a tournament, please enter a few details about the event.</p>
-
+        <InitForm />
       </div>
     )
   }
