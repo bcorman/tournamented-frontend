9e305832f504d4d17a44498a0bddebaa4ace8c7b
bcorman <benjamincorman@gmail.com>
2018-08-13 11:19:39 -0700

    initform has handlechange and partial submit implementation

--- src/components/forms/InitForm.js
+++ src/components/forms/InitForm.js
@@ -1,28 +1,45 @@
 import React, { Component } from 'react'
-import { Form, Button, Dropdown } from 'semantic-ui-react'
+import { Form, Button, Dropdown, Menu } from 'semantic-ui-react'
 
 class InitForm extends Component {
+  state = {}
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+  }
+
   render() {
+    const rounds = [
+      {key: 1, text: '1', value: 1},
+      {key: 2, text: '2', value: 2},
+      {key: 3, text: '3', value: 3},
+      {key: 4, text: '4', value: 4},
+      {key: 5, text: '5', value: 5}
+    ]
     return (
       <Form>
         <Form.Field>
           <label>Tournament Name</label>
-          <input placeholder='Tournament Name' />
+          <input name='tournamentName' placeholder='Tournament Name' onChange={this.handleChange}/>
         </Form.Field>
         <Form.Field>
           <label>Tournament Date</label>
-          <input placeholder='Tournament Date' />
+          <input name='tournamentDate' placeholder='Tournament Date' onChange={this.handleChange}/>
         </Form.Field>
         <Form.Field>
           <label>Number of Rounds</label>
-          <Dropdown options={[1, 2, 3, 4, 5]} />
+          <input name='tournamentLength' placeholder='fix this when you have internet' onChange={this.handleChange}/>
         </Form.Field>
         <Form.Field>
           <label>Tournament Location</label>
-          <input placeholder='Tournament Location' />
+          <input name='tournamentLocation' placeholder='Tournament Location' onChange={this.handleChange}/>
         </Form.Field>
-
-        <Button type='submit'>Submit</Button>
+        <Button type='submit' onClick={this.handleSubmit}>Submit</Button>
       </Form>
     )
   }
