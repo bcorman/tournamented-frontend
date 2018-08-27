77e0a7ffa65b8f0ffa7eb524eaf009547db866f9
bcorman <benjamincorman@gmail.com>
2018-08-27 01:00:30 -0500

    school detail form adds students to state

--- src/components/forms/SchoolDetailForm.js
+++ src/components/forms/SchoolDetailForm.js
@@ -2,18 +2,31 @@ import React, { Component } from 'react';
 import { Form, Button } from 'semantic-ui-react';
 
 class SchoolDetailForm extends Component {
-  state = {};
+  state = {
+    student: '',
+    students: [],
+  };
 
+  handleChange = (e) => {
+      this.setState({[e.target.name]: e.target.value})
+  };
+
+  addStudent = (e) => {
+    e.preventDefault();
+    if (this.state.students.length < 3) {
+      this.state.students.push(this.state.student);
+      this.setState({student: ''})
+    }
+    console.log(this.state)
+  }
   render() {
     return (
       <Form>
         <Form.Field>
-          <label>Add Student</label>
-          <span>
-            <input width={2} type='text' placeholder='Student Name' />
-            <Button>Add Student</Button>
-          </span>
+            <label>Add Student</label>
+            <input name='student' onChange={this.handleChange}/>
         </Form.Field>
+        <Button onClick={this.addStudent}>Submit</Button>
       </Form>
     )
   }
